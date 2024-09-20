e8a135b52e2bccb27127b1489f550a4ed9b94908
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-09-20 10:28:42 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: {
+        bgColor: "#",
+        myBlue: "#274C77",
+        myGrey: "#8B8C89",
+      },
+    },
+  },
+  plugins: [],
+};
