5d1d2d3c5e6dce374b1ce863b72e6a16382485fb
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-10-01 22:47:36 +0700

    setup react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl text-red-500 font-bold text-[50px]">
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
