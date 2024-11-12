bd0c1e0ef562c293dd4869ab5da898555e0d445f
Laurent <149299766+McLawrenc@users.noreply.github.com>
2024-11-12 15:37:30 +0700

    little update

--- client/src/screen/BookingData.js
+++ client/src/screen/BookingData.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Topnav from "../components/topnav";
-import Navbar from "../components/navbar";
+import Navbar from "../components/navbarUser";
 function BookingData() {
   return (
     <>
